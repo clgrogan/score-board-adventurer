2dbdb1386f7533059a9966939b87d4470134b319
Curt Grogan <curtgrogan@outlook.com>
2019-11-05 15:13:38 -0500

    Week 2 Day 2 Explorish

--- public/main.js
+++ public/main.js
@@ -4,42 +4,61 @@ const main = () => {
 
 // Define and assign global variables
 const incrementAmt = 1
+const maxScore = 3
+const initTeam1Name = 'Team 1'
+const initTeam2Name = 'Team 2'
+const initMyScoreBoard = 'My Score Board'
+let myScoreBoard = initMyScoreBoard
 let team1Score = 0
 let team2Score = 0
 let quarterNumber = 1
+let winner
+let team1Name = initTeam1Name
+let team2Name = initTeam2Name
 
-//Initialize Scores
+// Initialize Display Values
 document.querySelector('.team-1-score').textContent = team1Score
 document.querySelector('.team-2-score').textContent = team2Score
 document.querySelector('.quarter-number').textContent = quarterNumber
+document.querySelector('.h1-my-score-board').textContent = myScoreBoard
 
 // Update Team's Name When Button Clicked
 const updateTeam1Name = () => {
   console.log('clicked update team ONE NAME button')
-  const team1Name = document.querySelector('.team-1-input').value
+  team1Name = document.querySelector('.team-1-input').value
   console.log(team1Name)
   document.querySelector('.team-1-name').textContent = team1Name
 }
 const updateTeam2Name = () => {
   console.log('clicked update team TWO button')
-  const team2Name = document.querySelector('.team-2-input').value
+  team2Name = document.querySelector('.team-2-input').value
   console.log(team2Name)
   document.querySelector('.team-2-name').textContent = team2Name
 }
 
-// Update Teams' Score When Add Button Clicked
+// Functions - Update Teams' Score When Add Button Clicked
 const addTeam1Score = () => {
-  console.log('clicked update team 1 add S C O R E button')
-  team1Score = team1Score + incrementAmt
-  document.querySelector('.team-1-score').textContent = team1Score
+  if (team1Score < maxScore) {
+    console.log('clicked update team 1 add S C O R E button')
+    team1Score = team1Score + incrementAmt
+    document.querySelector('.team-1-score').textContent = team1Score
+  } else {
+    winner = team1Name
+    teamWins()
+  }
 }
 const addTeam2Score = () => {
   console.log('clicked update team TWO add S C O R E button ')
-  team2Score = team2Score + incrementAmt
-  document.querySelector('.team-2-score').textContent = team2Score
+  if (team2Score < maxScore) {
+    team2Score = team2Score + incrementAmt
+    document.querySelector('.team-2-score').textContent = team2Score
+  } else {
+    winner = team2Name
+    teamWins()
+  }
 }
 
-// Update Teams' Score When Subtract Button Clicked
+// Functions - Update Teams' Score When Subtract Button Clicked
 const subtractTeam1Score = () => {
   if (team1Score > 0) {
     console.log('clicked update team 1 subtract S C O R E button')
@@ -55,7 +74,7 @@ const subtractTeam2Score = () => {
   }
 }
 
-// Update Game Quarters
+// Functions Update Game Quarters Functions
 const subtractQuarter = () => {
   if (quarterNumber > 1) {
     console.log('clicked subtract quarter button')
@@ -67,6 +86,56 @@ const addQuarter = () => {
   console.log('clicked subtract quarter button')
   quarterNumber = quarterNumber + 1
   document.querySelector('.quarter-number').textContent = quarterNumber
+}
+
+// Functions Winning and
+const teamWins = () => {
+  // Display Winner
+  myScoreBoard = winner + ' WINS!!!'
+  console.log('Executed teamWins function. ', winner, myScoreBoard)
+  document.querySelector('.h1-my-score-board').textContent = myScoreBoard
+
+  // Disable Buttons
+  document.querySelector('.team-1-add-1-button').disabled = true
+  document.querySelector('.team-1-subtract-1-button').disabled = true
+  document.querySelector('.team-2-add-1-button').disabled = true
+  document.querySelector('.team-2-subtract-1-button').disabled = true
+  document.querySelector('.update-team-1-name').disabled = true
+  document.querySelector('.update-team-2-name').disabled = true
+  document.querySelector('.subtract-qtr-btn').disabled = true
+  document.querySelector('.add-qtr-btn').disabled = true
+}
+
+// Initialize or reset
+const resetAll = () => {
+  // Initialize Global Variables
+  myScoreBoard = initMyScoreBoard
+  team1Score = 0
+  team2Score = 0
+  quarterNumber = 1
+  winner = ''
+  team1Name = initTeam1Name
+  team2Name = initTeam2Name
+
+  // Reset scoreboard
+  document.querySelector('.team-1-score').textContent = team1Score
+  document.querySelector('.team-2-score').textContent = team2Score
+  document.querySelector('.quarter-number').textContent = quarterNumber
+  document.querySelector('.h1-my-score-board').textContent = myScoreBoard
+  document.querySelector('.team-1-name').textContent = team1Name
+  document.querySelector('.team-1-input').value = ''
+  document.querySelector('.team-2-name').textContent = team2Name
+  document.querySelector('.team-2-input').value = ''
+
+  // Enable Buttons
+  document.querySelector('.team-1-add-1-button').disabled = false
+  document.querySelector('.team-1-subtract-1-button').disabled = false
+  document.querySelector('.team-2-add-1-button').disabled = false
+  document.querySelector('.team-2-subtract-1-button').disabled = false
+  document.querySelector('.update-team-1-name').disabled = false
+  document.querySelector('.update-team-2-name').disabled = false
+  document.querySelector('.subtract-qtr-btn').disabled = false
+  document.querySelector('.add-qtr-btn').disabled = false
 }
 
 // Add listeners
@@ -98,4 +167,7 @@ document
 document
   .querySelector('.subtract-qtr-btn')
   .addEventListener('click', subtractQuarter)
+
 document.querySelector('.add-qtr-btn').addEventListener('click', addQuarter)
+
+document.querySelector('.reset-button').addEventListener('click', resetAll)
